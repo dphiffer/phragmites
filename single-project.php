ffcfd392abb2aa9d376af08d70c336c99eb0de77
Dan Phiffer <dan@phiffer.org>
2023-02-22 22:10:51 -0500

    add project social cards

--- single-project.php
+++ single-project.php
@@ -1,31 +1,47 @@
 <?php
 
-if (get_field('project_website')) {
-	$url = get_field('project_url');
-	header("Location: $url");
-	exit;
-}
+global $post, $phragmites;
 
-get_header();
+while (have_posts()) {
+	the_post();
 
-global $post;
-$breadcrumbs = [];
-$curr = $post;
-$breadcrumbs[$curr->post_title] = get_permalink($curr->ID);
-while ($curr->post_parent) {
-	$curr = get_post($curr->post_parent);
+	if (get_field('project_website')) {
+		$url = get_field('project_url');
+		header("Location: $url");
+		exit;
+	}
+
+	$phragmites->meta['title'] = $post->post_title . ' - ' . get_bloginfo('name');
+	$excerpt = wp_trim_excerpt('', $post);
+	if (! empty($excerpt)) {
+		$phragmites->meta['description'] = $excerpt;
+	}
+	$image_id = get_post_thumbnail_id($post->ID);
+	$phragmites->meta['twitter_image'] = $phragmites->image_url($image_id, 'twitter');
+	$phragmites->meta['facebook_image'] = $phragmites->image_url($image_id, 'facebook');
+
+	get_header();
+
+	$breadcrumbs = [];
+	$curr = $post;
 	$breadcrumbs[$curr->post_title] = get_permalink($curr->ID);
-}
-$breadcrumbs['Projects'] = '/projects';
-$breadcrumbs[get_bloginfo('name')] = '/';
+	while ($curr->post_parent) {
+		$curr = get_post($curr->post_parent);
+		$breadcrumbs[$curr->post_title] = get_permalink($curr->ID);
+	}
+	$breadcrumbs['Projects'] = '/projects';
+	$breadcrumbs[get_bloginfo('name')] = '/';
 
-?>
-<section class="project-page <?php echo @$project_class; ?>">
-	<?php echo $phragmites->get_breadcrumbs($breadcrumbs); ?>
-	<div class="container">
-		<div class="content">
-			<?php the_content(); ?>
+	?>
+	<section class="project-page <?php echo @$project_class; ?>">
+		<?php echo $phragmites->get_breadcrumbs($breadcrumbs); ?>
+		<div class="container">
+			<div class="content">
+				<?php the_content(); ?>
+			</div>
 		</div>
-	</div>
-</section>
-<?php get_footer();
+	</section>
+	<?php
+}
+
+get_footer();
